e701a1cb66d1c116d5557ac4015eb1a8c5884ce6
&Rewards <88257984+andrewards@users.noreply.github.com>
2021-08-01 12:32:34 -0300

    Usando o nodemon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/atendimentos', (req, res) => {
-    res.send('Você está na rota de atendimentos!');
+    res.send('Você está na rota de atendimentos e está realizando um GET!');
 });
 
 app.listen(3000, () => {
