965bfa7611f37bc6fad4669e51d4a56e4b934ea4
&Rewards <88257984+andrewards@users.noreply.github.com>
2021-08-01 13:27:25 -0300

    Rota para criar atendimento

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -4,4 +4,8 @@ module.exports = app => {
         res.send('Você está na rota de atendimentos e está realizando um GET!');
     });
 
+    app.post('/atendimentos', (req, res) => {
+        res.send('Você está na rota de atendimentos e está realizando um POST!')
+    });
+
 }
