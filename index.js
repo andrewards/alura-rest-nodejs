a2f5603b3bfa8637a35b22ec201a24432f3160ea
&Rewards <88257984+andrewards@users.noreply.github.com>
2021-08-01 21:09:03 -0300

    Criando as tabelas

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const customExpress = require('./config/customExpress');
 const connection = require('./infraestrutura/connection');
+const tables = require('./infraestrutura/tabelas');
 
 connection.connect(err => {
     if (err) console.log(err);
     else {
         console.log('Conectado ao Banco de Dados!');
+
+        tables.init(connection);
         const app = customExpress();
 
         app.listen(3000, () => console.log('Servidor rodando na porta 3000...'));
---
+++ infraestrutura/tabelas.js
@@ -0,0 +1,22 @@
+class Tables {
+
+    init(connection) {
+        this.connection = connection;
+        console.log('As tabelas foram chamadas...');
+
+        this.criarAtendimentos();
+    }
+
+    criarAtendimentos() {
+
+        const q = 'CREATE TABLE atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))';
+
+        this.connection.query(q, err => {
+            if (err) console.log(err);
+            else console.log("Tabela 'atendimentos' criada com sucesso!");
+        });
+    }
+
+}
+
+module.exports = new Tables;
