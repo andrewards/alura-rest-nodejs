d1f9df6caab87021bb99cd7f83f6fa8bb3a8e85b
&Rewards <88257984+andrewards@users.noreply.github.com>
2021-08-01 22:13:36 -0300

    Aplicando validações de entrada

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -3,17 +3,39 @@ const connection = require('../infraestrutura/connection');
 class Atendimentos {
 
     create(atendimento, res) {
-        
-        const q = "INSERT INTO atendimentos SET ?";
 
         const dataCriacao = new Date();
         const data = new Date(atendimento.data);
-        const atendimentoDatado = {...atendimento, dataCriacao, data};
 
-        connection.query(q, atendimentoDatado, (err, result) => {
-            if (err) res.status(400).json(err);
-            else res.status(201).json(result);
-        });
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: data.getTime() > dataCriacao.getTime(),
+                mensagem: 'Informe uma data futura',
+            },
+            {
+                nome: 'cliente',
+                valido: atendimento.cliente.length >= 5,
+                mensagem: 'O cliente deve ter pelo menos 5 caracteres'
+            }
+        ];
+
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros = erros.length;
+
+        if (existemErros) {
+            res.status(400).json(erros);
+        } else {
+
+            const atendimentoDatado = {...atendimento, dataCriacao, data};
+
+            const q = "INSERT INTO atendimentos SET ?";
+            connection.query(q, atendimentoDatado, (err, result) => {
+                if (err) res.status(400).json(err);
+                else res.status(201).json(result);
+            });
+
+        }
 
     }
 
